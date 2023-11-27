ed29e86b44509f7f166dc6bf46de0ec41c6e7f18
Giggle1998 <rlvy98@naver.com>
2023-11-27 21:24:04 +0900

    feat : COS_PRO_3rd_6

---
+++ 3rd/COS_PRO_3rd_6.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+class Main {
+    public int solution(int n) {
+        int answer = 0;
+        ArrayList<Integer> primes = new ArrayList<Integer>();
+        primes.add(2);
+        for (int i = 3; i <= n; i += 2) {
+            boolean isPrime = true;
+            for (int j = 2; j < i; j++)
+                if (i % j == 0){
+                    isPrime = false;
+                    break;
+                }
+            if (isPrime)
+                primes.add(i);
+        }
+        
+        int primeLen = primes.size();
+        for (int i = 0; i < primeLen - 2; i++)
+            for (int j = i + 1; j < primeLen - 1; j++)
+                for (int k = j + 1; k < primeLen; k++)
+                    if (primes.get(i) + primes.get(j) + primes.get(k) == n)
+                        answer++;
+        return answer;
+    }
+    public static void main(String[] args) {
+    	Main sol = new Main();
+        int n1 = 33;
+        int ret1 = sol.solution(n1);
+        System.out.println("solution 메소드의 반환 값은 " + ret1 + " 입니다.");
+        
+        int n2 = 9;
+        int ret2 = sol.solution(n2);
+        System.out.println("solution 메소드의 반환 값은 " + ret2 + " 입니다.");
+    }    
+}
+
