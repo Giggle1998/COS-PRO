9d4fb40a750cbfba8b527dc50963638e2d952744
Giggle1998 <rlvy98@naver.com>
2023-11-28 23:38:35 +0900

    feat : COS_PRO_3rd_7

---
+++ 3rd/COS_PRO_3rd_7.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Main {
+    public int[] solution(int k) {
+        int[] kaprekarArr = new int[k];
+        int count = 0;
+        for (int i = 1; i <= k; i++) {
+        	long squareNum = i * i;
+        	long divisor = 1;
+        	while (squareNum / divisor != 0) {
+        		long front = squareNum / divisor;
+        		long back = squareNum % divisor;
+        		divisor *= 10;
+        		if (back != 0 && front != 0)
+        			if (front + back == i) {
+        				kaprekarArr[count] = i;
+        				count++;
+        			}
+        	}
+        }
+        int[] answer = new int[count];
+        for (int i = 0; i < count; i++) 
+        	answer[i] = kaprekarArr[i];
+        return answer;
+    }
+    public static void main(String[] args) {
+        Main sol = new Main();
+        int k = 500;
+        int[] ret = sol.solution(k);
+        System.out.println("solution 메소드의 반환 값은 " + Arrays.toString(ret) + " 입니다.");
+    }
+}
+
