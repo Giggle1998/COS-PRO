be3cc8104465c4ca1a9c1471e6da0f899f6a8898
Giggle1998 <rlvy98@naver.com>
2023-11-29 16:47:57 +0900

    feat : COS_PRO_5th_4

---
+++ 5th/COS_PRO_5th_4.java
@@ -0,0 +1,30 @@
+class Main {
+    public String solution(int number) {
+        String answer = "";
+
+        int[] numberCount = new int[10];
+        while(number > 0) {
+            numberCount[number % 10]++;
+            number /= 10;
+        }
+
+        for(int i = 9; i >= 0; i--)
+            if(numberCount[i] != 0)
+                answer += (String.valueOf(i) + String.valueOf(numberCount[i]));
+        
+        return answer;
+    }
+
+    public static void main(String[] args) {
+    	Main sol = new Main();
+        
+    	int number1 = 2433;
+    	String ret1 = sol.solution(number1);
+    	System.out.println("solution 메소드의 반환 값은 \"" + ret1 + "\" 입니다.");
+
+    	int number2 = 662244;
+    	String ret2 = sol.solution(number2);
+    	System.out.println("solution 메소드의 반환 값은 \"" + ret2 + "\" 입니다.");
+    }
+}
+
